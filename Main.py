c40d0a1d9b9c46b0c7123ca748ee0f94bfa752be
Lenor <icuvip@qq.com>
2024-12-06 16:02:36 +0800

    np acc

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from SetParam import *
 from ProgBar import progress_bar
 
@@ -7,30 +9,51 @@ def get_i_real(uo, i_in):
 
 
 def calc_ic(u_left, u_right, u_this):
-    return (u_left - u_this) / R_sis + (u_right - u_this) / R_sis + (up - u_this) / R_leak
+    return (u_left - u_this + u_right - u_this) / R_sis + (up - u_this) / R_leak
+
 
 def solve_i():
     i_real = get_i_real(u_n[0], I_in)
     i_n[0] = i_real + calc_ic(u_n[0], u_n[1], u_n[0])
-    for i in range(1, n-1):
-        i_n[i] = calc_ic(u_n[i-1], u_n[i+1], u_n[i])
+    for i in range(1, n - 1):
+        i_n[i] = calc_ic(u_n[i - 1], u_n[i + 1], u_n[i])
     i_n[-1] = calc_ic(u_n[-2], u_n[-1], u_n[-1])
+
 
 def solve_u():
     for i in range(n):
         u_n[i] += dt * i_n[i] / C
 
+def solve_i_np():
+    i_real = get_i_real(u_n_np[0], I_in)
+    i_n_np[1:-1] = (u_n_np[:-2] - u_n_np[1:-1] + u_n_np[2:] - u_n_np[1:-1]) / R_sis + (up - u_n_np[1:-1]) / R_leak
+    i_n_np[0] = i_real + calc_ic(u_n_np[0], u_n_np[1], u_n_np[0])
+    i_n_np[-1] = calc_ic(u_n_np[-2], u_n_np[-1], u_n_np[-1])
 
+
+def solve_u_np():
+    for i in range(n):
+        u_n_np[i] += dt * i_n_np[i] / C
 
 
 if __name__ == '__main__':
     u_n = [i + 15 for i in range(n)]
     i_n = [0 for i in range(n)]
-    if T/dt != int(T/dt):
+    u_n_np = np.array(u_n, dtype=np.float64)
+    if np_mode:
+        i_n_np = np.array(i_n, dtype=np.float64)
+    if T / dt != int(T / dt):
         print("T/dt must be an integer")
-    for itime in range(int(T/dt)):
-        solve_i()
-        solve_u()
-        progress_bar(itime / (T/dt) * 100)
+    for itime in range(int(T / dt)):
+        progress_bar(itime / (T / dt) * 100)
+        if np_mode:
+            solve_i_np()
+            solve_u_np()
+        else:
+            solve_i()
+            solve_u()
     print()
-    print(u_n)
+    if np_mode:
+        print(u_n_np)
+    else:
+        print(u_n)
--- SetParam.py
+++ SetParam.py
@@ -1,8 +1,8 @@
 C = 1
 R_sis = 0.1
-R_leak = 100000000
-R_leak_L = 100000000
-R_leak_R = 100000000
+R_leak = 100000
+R_leak_L = 100000
+R_leak_R = 100000
 
 I_in = 0
 up = 25
@@ -10,3 +10,6 @@ up = 25
 n = 100
 dt = 0.001
 T = 1000.0
+
+np_mode = True
+pre = 1000000
